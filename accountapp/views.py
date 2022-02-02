e1a9cbdc25bbcbb0a1ae19d7e496f09ff1a54af5
Va11-Ha11a <taesoo3880@gmail.com>
2022-02-02 15:42:02 +0900

    usual

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from accountapp.models import Corgi
+
 
 def hello_world(request):
 
     if request.method == "POST":
 
-      return render(request, 'accountapp/hello_world.html', context={'text': 'LIT!'})
+        temp = request.POST.get('doggo_input')
+
+        welsh_corgi = Corgi()
+        welsh_corgi.text = temp
+        welsh_corgi.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'corgi_output': welsh_corgi})
 
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'Do U like doggo?'})
